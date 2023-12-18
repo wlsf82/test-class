ffffbb27043c865acadc4abcb28380cf6af0becd
Walmyr <wlsf82@gmail.com>
2023-12-18 17:06:47 +0100

    Install and configure cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require('cypress')
 
+const cypressSplit = require('cypress-split')
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      cypressSplit(on, config)
+
+      return config
     },
   },
 })
